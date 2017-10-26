ca9d8a063d0e04cd162f3a09d1f127441b84996a
1 <1>
2017-10-26 09:34:13 +0800

    edit

--- Deep_Learning_with_TensorFlow/1.0.0/Chapter08/reader_bak.py
+++ Deep_Learning_with_TensorFlow/1.0.0/Chapter08/reader_bak.py
@@ -21,14 +21,13 @@ from __future__ import print_function
 
 import collections
 import os
-import sys
 
 import tensorflow as tf
 
 
 def _read_words(filename):
   with tf.gfile.GFile(filename, "r") as f:
-    return f.read().replace("\n", "<eos>").split()
+    return f.read().decode("utf-8").replace("\n", "<eos>").split()
 
 
 def _build_vocab(filename):
